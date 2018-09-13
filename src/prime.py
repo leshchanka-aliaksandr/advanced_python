eb7a61cf338651a1e5cbc58f3c3487c52a18aba6
Aliaksandr, Leshchanka EX2 <laliaksandr@us.imshealth.com>
2018-09-13 13:34:03 +0300

    asyncio prime

---
+++ src/prime.py
@@ -0,0 +1,25 @@
+import asyncio
+import math
+
+
+async def is_prime(x, result):
+    for i in range(2, int(math.sqrt(x))):
+        if i % 100 == 0:
+            await asyncio.sleep(1)
+        if x % i == 0:
+            return
+    result[0] += x
+    return
+
+
+if __name__ == "__main__":
+    n = 10
+    m = 1000000
+    loop = asyncio.get_event_loop()
+    result = [0]
+    loop.run_until_complete(asyncio.gather(
+        *[is_prime(i, result) for i in range(n, m)]
+    )
+    )
+    loop.close()
+    print(result[0])
