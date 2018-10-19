6d6a403b1e29f2f13606b52bf10457a7b7cf765c
Aliaksandr, Leshchanka EX2 <laliaksandr@us.imshealth.com>
2018-10-19 15:45:57 +0300

    create setup.py file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name="currency",
+      version="0.1",
+      description="Currency calculator",
+      author="Alex Leschehnko",
+      author_email="leshchanka.aliaksandr@gmail.com",
+      packages=["currency"],
+      install_requires=['requests'])
